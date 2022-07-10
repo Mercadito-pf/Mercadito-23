2cc025684b09b40f378e1027eb320611473f2a52
Andres <afguerreroleal12@gmail.com>
2022-07-09 21:47:14 -0500

    Se modifica force

--- api/index.js
+++ api/index.js
@@ -1,15 +1,11 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { chargue } = require("./src/init/preChargueFile.js");
-const { Product } = require("./src/db");
 /**
  * @author Nicolas Alejandro Suarez
  */
-conn.sync({ force: false }).then(async () => {
-  let products = await Product.findAll();
-  if (!products) {
-    await chargue();
-  }
+conn.sync({ force: true }).then(async () => {
+  await chargue();
   server.listen(3001, () => {
     console.log("%s listening at 3001");
   });
