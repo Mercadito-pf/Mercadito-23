3f3b64a8e95f88a4009653be1b9214dd069ff5bd
wilbernp <wilbernarvaezpetro@gmail.com>
2022-07-16 10:59:15 -0500

    agregando estilos al front y creando ruta de favoritos

---
+++ client/src/Componentes/Card/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import './Card.scss'
+
+
+const Cards = (products)=>{
+    return (
+        <div className = 'principalContainer'>
+            
+            <div className = 'cardContent'>
+                <div className = 'image'>
+                    <img src={products.image} alt='img not found' />
+                </div>
+                
+                <div className = 'cardText'>
+                    <h3>{products.name}</h3>
+                    <h4>${products.price}.00</h4>
+                    <h2>{products.seller}</h2>
+                    <p>{products.sales} ventas</p>
+                    <Link to={`/detail/${products.id}`} className='detailsButton'>
+                        <button>see more</button>
+                    </Link>
+                </div>
+                
+            </div>
+        
+        </div>
+    )
+}
+
+export default Cards;
