4233591d086445298b9d098a27f8a7a12c9ef82d
wilbernp <wilbernarvaezpetro@gmail.com>
2022-07-27 09:05:17 -0500

    panel de admin

---
+++ admin/src/components/datatable/Datatable.jsx
@@ -0,0 +1,76 @@
+import "./datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumns } from "../../datatablesource";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Datatable = () => {
+  const [data, setData] = useState([]);
+  let [refres, setRefres]=useState(true)
+  useEffect(() => {
+    fetch(`http://localhost:3001/users/user-list`)
+        .then((response) => response.json())
+        .then((json) => {
+            let map_id =json.map(user=>{
+              return{...user, id:user._id}
+            })
+            setData(map_id)
+        })
+        .catch((e) => console.log(e))
+}, [refres]);
+
+  async function handleBan (id){
+     await fetch(`http://localhost:3001/users/ban-user/${id}`,{
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({status:"baned"})
+    })
+
+    setRefres(!refres)
+  }
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        console.log(params)
+        return (
+          <div className="cellAction">
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
+            <div
+              className="deleteButton"
+              onClick={() => handleBan(params.row._id)}
+            >
+              Ban
+            </div>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="datatable">
+      <div className="datatableTitle">
+        Users
+        
+      </div>
+      <DataGrid
+        className="datagrid"
+        rows={data}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        // checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default Datatable;
