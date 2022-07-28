20c22174523de4486600b070fbc652fbdb5f60f4
CarlaSalHua <c.salhua@gmail.com>
2022-07-28 13:05:59 -0500

    ultimo

--- client/src/Componentes/Fav/ListFav.jsx
+++ client/src/Componentes/Fav/ListFav.jsx
@@ -41,7 +41,7 @@ export default function ListFav() {
         { state.length? state.map(p =>{
                 if (p.product) {
                     return(
-                        <Cards id_fav={p._id} list={true} handleClick={handleClick} image={p.product.image} name={p.product.name} seller={p.product.seller} sales={p.product.sales} price={p.product.price} id={p.product.id}/>
+                        <Cards  id_fav={p._id} list={true} handleClick={handleClick} image={p.product.image} name={p.product.name} seller={p.product.seller} sales={p.product.sales} price={p.product.price} id={p.product.id}/>
                     )
                 }
             }): <h1 className='textFav'>No tienes productos en tu seccion de favoritos</h1>
