6205260a8f4c6a87dbce892db37548007cfef09c
CarlaSalHua <c.salhua@gmail.com>
2022-07-28 09:53:44 -0500

    styles

--- client/src/Componentes/Orden/Orden.jsx
+++ client/src/Componentes/Orden/Orden.jsx
@@ -6,6 +6,8 @@ import {Link,useParams} from 'react-router-dom';
 import { ordenP } from '../FormBuy/FormBuy';
 import Paypal from '../Paypal/Paypal';
 
+import './Orden.scss'
+
 export default function Orden() {
   const[ordernP, setOrdenP] = useState({})
   let id_cart = localStorage.getItem("id_cart")
@@ -26,33 +28,48 @@ export default function Orden() {
 
   //  }  
     return (
-        <>
-            {
-              ordernP.products && ordernP.products.map(product =>(
-                <div>
-                    <h3>Producto: {product.name}<br/>Cantidad: {product.cantidad}<br/>Stock: {product.stock}<br/>Price: {product.price}</h3>,
+        <div className='ordenContainer'>
+          <div className='subOrden'>
+              {
+                ordernP.products && ordernP.products.map(product =>(
+                  <div className='it'>
+                      <h2>Producto: {product.name}</h2>
+                      <h3>Cantidad: {product.cantidad}</h3>
+                      <h3>Stock: {product.stock}</h3>
+                      <h3>Price: {product.price}</h3>
                   </div>
-                ))
-              }
-          {
-            ordernP.user &&
-           <div>
-             <h3>Nombre: <br/>{ordernP.user.nombre}<br/>Apellido:<br/> {ordernP.user.apellido}<br/>Direccion:<br/> {ordernP.user.direccion}<br/>Codigo Postal:<br/> {ordernP.user.codigoPostal}<br/>Ciudad: <br/>{ordernP.user.ciudad}<br/>Pais:<br/> {ordernP.user.pais}<br/>Telefono:<br/> {ordernP.user.telefono}<br/></h3>
-           </div>
-         } 
-         <br/>
-          {
-           ordernP.calc &&
-            <div>
-              <h3>Subtotal: {ordernP.calc.subtotal}<br/>Total Products: {ordernP.calc.total}<br/> Impuestos: {ordernP.calc.impuestos}<br/> Precio Total: {ordernP.calc.totalPrice}</h3>
+                  ))
+                }
+            {
+              ordernP.user &&
+            <div className='user'>
+              <h3><a>Nombre:</a> {ordernP.user.nombre}</h3>
+              <h3><a>Apellido:</a> {ordernP.user.apellido}</h3>
+              <h3><a>Direccion:</a> {ordernP.user.direccion}</h3>
+              <h3><a>Codigo Postal:</a> {ordernP.user.codigoPostal}</h3>
+              <h3><a>Ciudad:</a> {ordernP.user.ciudad}</h3>
+              <h3><a>Pais:</a> {ordernP.user.pais}</h3>
+              <h3><a>Telefono:</a> {ordernP.user.telefono}</h3>
             </div>
           } 
+        
+            {
+            ordernP.calc &&
+              <div className='calc'>
+                <h3><a>Subtotal:</a> {ordernP.calc.subtotal}</h3>
+                <h3><a>Total Products:</a> {ordernP.calc.total}</h3>
+                <h3><a>Impuestos:</a> $ {ordernP.calc.impuestos}</h3>
+                <h3><a>Precio Total:</a> $ {ordernP.calc.totalPrice}</h3>
+              </div>
+            } 
+
+            <Paypal/>
+          </div>
                 {/* subtotal:Number, 
         total:Number,
         impuestos:Number, 
         totalPrice:Number, */}
-        <Paypal/>
-        </>
+        </div>
 
     )
 }
